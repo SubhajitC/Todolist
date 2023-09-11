604c05e31a76dc416cd3f9ade552760a3232746f
Subhajit Chakraborty <subhajitc245@gmail.com>
2023-09-11 02:10:49 -0700

    backendfolderstructure

--- todoapp/src/screens/LoginScreen.js
+++ todoapp/src/screens/LoginScreen.js
@@ -12,26 +12,31 @@ const LoginScreen = ({ navigation }) => {
     try {
       // Validate email and password
       if (!email || !password) {
-        // Handle validation error
+        console.log('Validation error: Email and password are required.'); // Add this line for validation check
         return;
       }
-
+  
       const userData = {
         email,
         password,
       };
-
+  
+      console.log('Attempting to log in with userData:', userData); // Add this line to log userData
+  
       const user = await dispatch(loginUser(userData));
-
+  
+      console.log('Login response:', user); // Add this line to log the user response
+  
       if (user) {
-        // Handle successful login, e.g., navigate to HomeScreen
+        console.log('User successfully logged in:', user); // Add this line to check if user is logged in
         navigation.navigate('Home');
       } else {
+        console.log('Login failed: User is not logged in.'); // Add this line for login failure
         // Handle login failure, show an error message, etc.
       }
     } catch (error) {
+      console.error('Login error:', error); // Add this line to log API request errors
       // Handle API request error
-      console.error('Login error:', error);
     }
   };
 
